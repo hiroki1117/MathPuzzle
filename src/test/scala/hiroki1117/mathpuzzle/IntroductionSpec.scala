0abfbedf5ef7c7559acf267e0add3837e5f334b1
hiroki <scala1117@gmail.com>
2019-01-05 14:08:00 +0900

    テスト名を入門(Introduction)に変更

--- src/test/scala/hiroki1117/mathpuzzle/IntroductionSpec.scala
+++ src/test/scala/hiroki1117/mathpuzzle/IntroductionSpec.scala
@@ -3,7 +3,7 @@ package hiroki1117.mathpuzzle
 import org.scalatest._
 import Matchers._
 
-class PSpec extends FlatSpec {
+class IntroductionSpec extends FlatSpec {
 
   "Q01" should "answer 585" in {
     Q01.answer(10) should be (585)
